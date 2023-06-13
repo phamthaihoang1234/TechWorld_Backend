06b2df8299e60f7981135921fbcbd71ef102e2eb
tiennq <tiennqhe153125@fpt.edu.vn>
2023-06-13 12:07:25 +0700

    Edit folder

--- src/main/java/com/example/TechWorld/service/implement/ProductServiceImplement.java
+++ src/main/java/com/example/TechWorld/service/implement/ProductServiceImplement.java
@@ -1,17 +1,16 @@
-package com.example.TechWorld.serviceImpl;
-
+package com.example.TechWorld.service.implement;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.example.TechWorld.model.Product;
 import com.example.TechWorld.repository.ProductRepository;
 import com.example.TechWorld.service.ProductService;
 
-@Component
-public class ProductServiceImpl implements ProductService {
+@Service
+public class ProductServiceImplement implements ProductService {
 
     @Autowired
     private ProductRepository productRepo;
