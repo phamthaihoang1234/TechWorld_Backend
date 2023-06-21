9d5052a3bedc0d8b7bff4c40f186e50799a5450b
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 16:29:08 +0700

    add findAllOrder for OrderApiHandle

--- src/main/java/com/example/TechWorld/apiHandle/OrderApiHandle.java
+++ src/main/java/com/example/TechWorld/apiHandle/OrderApiHandle.java
@@ -1,11 +1,17 @@
 package com.example.TechWorld.apiHandle;
 
 
+import com.example.TechWorld.model.Order;
 import com.example.TechWorld.repository.*;
+import com.example.TechWorld.utils.SendEmail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -13,23 +19,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderApiHandle {
 
     @Autowired
-    private OrderRepository orderRepo;
+    private OrderRepository orderRepository;
 
     @Autowired
-    private OrderDetailRepository orderDetailRepo;
+    private OrderDetailRepository orderDetailRepository;
 
     @Autowired
-    private UserRepository userRepo;
+    private UserRepository userRepository;
 
     @Autowired
-    private CartRepository cartRepo;
+    private CartRepository cartRepository;
 
     @Autowired
-    private CartDetailRepository cartDetailRepo;
+    private CartDetailRepository cartDetailRepository;
 
     @Autowired
-    private ProductRepository productRepo;
+    private ProductRepository productRepository;
 
-    /*@Autowired
-    private SendMailUtil sendMailUtil;*/
+    @Autowired
+    private SendEmail sendMailUtil;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAllOrder() {
+        List<Order> orders = orderRepository.findAllByOrderByOrdersIdDesc();
+        return ResponseEntity.ok(orders);
+    }
+
+
 }
