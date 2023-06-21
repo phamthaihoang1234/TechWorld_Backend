7076caa3d9d8345bf5231fb30f82c20bbc157bf7
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 15:27:36 +0700

    add CartApiHanle for getCartUser

--- src/main/java/com/example/TechWorld/apiHandle/CartApiHandle.java
+++ src/main/java/com/example/TechWorld/apiHandle/CartApiHandle.java
@@ -1,14 +1,37 @@
 package com.example.TechWorld.apiHandle;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.TechWorld.model.Cart;
+import com.example.TechWorld.repository.CartDetailRepository;
+import com.example.TechWorld.repository.CartRepository;
+import com.example.TechWorld.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("api/cart")
 public class CartApiHandle {
+
+    private final CartRepository cartRepository;
+    private final CartDetailRepository cartDetailRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CartApiHandle(CartRepository cartRepository, CartDetailRepository cartDetailRepository, UserRepository userRepository) {
+        this.cartRepository = cartRepository;
+        this.cartDetailRepository = cartDetailRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/{email}")
+    public ResponseEntity<Cart> getCartUser(@PathVariable("email") String email) {
+        if (!userRepository.existsByEmail(email)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cartRepository.findByUser(userRepository.findByEmail(email).get()));
+    }
 
 
 
