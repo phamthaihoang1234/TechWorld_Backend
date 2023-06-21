1e748610c72d5531b5f86d30ef23c9db294074e0
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 15:35:18 +0700

    add getOneCartDetail for CartDetailApiHanle

--- src/main/java/com/example/TechWorld/apiHandle/CartDetailApiHandle.java
+++ src/main/java/com/example/TechWorld/apiHandle/CartDetailApiHandle.java
@@ -1,13 +1,13 @@
 package com.example.TechWorld.apiHandle;
 
 
+import com.example.TechWorld.model.CartDetail;
 import com.example.TechWorld.repository.CartDetailRepository;
 import com.example.TechWorld.repository.CartRepository;
 import com.example.TechWorld.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
@@ -23,6 +23,14 @@ public class CartDetailApiHandle {
         this.cartDetailRepository = cartDetailRepository;
         this.cartRepository = cartRepository;
         this.productRepository = productRepository;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CartDetail> getOneCartDetail(@PathVariable("id") Long id) {
+        if (!cartDetailRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cartDetailRepository.findById(id).get());
     }
 
 }
