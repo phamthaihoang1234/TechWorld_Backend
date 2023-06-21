c76e8f4bab50ed47bcf16d1e3d3e5f142804d28a
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 15:26:01 +0700

    add CartApiHanle

---
+++ src/main/java/com/example/TechWorld/apiHandle/CartApiHandle.java
@@ -0,0 +1,15 @@
+package com.example.TechWorld.apiHandle;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("api/cart")
+public class CartApiHandle {
+
+
+
+}
