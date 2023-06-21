316c98f9e4be65deaa2edf4476979d91b183b0c5
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 15:12:25 +0700

    add ProductApiHanle for latest product

--- src/main/java/com/example/TechWorld/apiHandle/ProductApiHanle.java
+++ src/main/java/com/example/TechWorld/apiHandle/ProductApiHanle.java
@@ -1,12 +1,17 @@
 package com.example.TechWorld.apiHandle;
 
 
+import com.example.TechWorld.model.Product;
 import com.example.TechWorld.repository.CategoryRepository;
 import com.example.TechWorld.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -20,6 +25,24 @@ public class ProductApiHanle {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+
+        List<Product> products = productRepository.findByStatusTrue();
+        return ResponseEntity.ok(products);
+
+    }
+
+    @GetMapping("latest")
+    public ResponseEntity<List<Product>> getLatestProducts(){
+        List<Product> products = productRepository.findByStatusTrueOrderByEnteredDateDesc();
+        return ResponseEntity.ok(products);
+
+    }
+
+
 
 
 
