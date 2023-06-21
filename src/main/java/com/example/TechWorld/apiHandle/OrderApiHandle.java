1dc9f2b1a770efca9307b15ed69fe0e4554ed69b
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 16:21:33 +0700

    add OrderApiHandle Repository

--- src/main/java/com/example/TechWorld/apiHandle/OrderApiHandle.java
+++ src/main/java/com/example/TechWorld/apiHandle/OrderApiHandle.java
@@ -1,4 +1,35 @@
 package com.example.TechWorld.apiHandle;
 
+
+import com.example.TechWorld.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("api/orders")
 public class OrderApiHandle {
+
+    @Autowired
+    private OrderRepository orderRepo;
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepo;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private CartRepository cartRepo;
+
+    @Autowired
+    private CartDetailRepository cartDetailRepo;
+
+    @Autowired
+    private ProductRepository productRepo;
+
+    /*@Autowired
+    private SendMailUtil sendMailUtil;*/
 }
