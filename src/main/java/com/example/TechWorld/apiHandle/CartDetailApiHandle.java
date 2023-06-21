39d2466ae04b394e052e44289acd5c3f79e5dc3f
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-21 15:34:25 +0700

    add DI CartDetailApiHanle

--- src/main/java/com/example/TechWorld/apiHandle/CartDetailApiHandle.java
+++ src/main/java/com/example/TechWorld/apiHandle/CartDetailApiHandle.java
@@ -1,6 +1,10 @@
 package com.example.TechWorld.apiHandle;
 
 
+import com.example.TechWorld.repository.CartDetailRepository;
+import com.example.TechWorld.repository.CartRepository;
+import com.example.TechWorld.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/cartDetail")
 public class CartDetailApiHandle {
+
+    private final CartDetailRepository cartDetailRepository;
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public CartDetailApiHandle(CartDetailRepository cartDetailRepository, CartRepository cartRepository, ProductRepository productRepository) {
+        this.cartDetailRepository = cartDetailRepository;
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+    }
+
 }
