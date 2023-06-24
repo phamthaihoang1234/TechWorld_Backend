7cb14fe27c8200852cd0d8c7c6d82e72d545bc3a
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-06-24 12:51:11 +0700

    fix CorsConfig

--- src/main/java/com/example/TechWorld/configuration/CorsConfig.java
+++ src/main/java/com/example/TechWorld/configuration/CorsConfig.java
@@ -1,4 +1,30 @@
 package com.example.TechWorld.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
 public class CorsConfig {
+
+
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
