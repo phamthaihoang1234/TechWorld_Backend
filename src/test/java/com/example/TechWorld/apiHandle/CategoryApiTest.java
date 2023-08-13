4029e21a22f5a81fae95e1eb8353afb6b5038e0f
tiennq <97077135+NguyenQueTien@users.noreply.github.com>
2023-08-13 12:09:52 +0700

    Change name test class

--- src/test/java/com/example/TechWorld/apiHandle/CategoryApiTest.java
+++ src/test/java/com/example/TechWorld/apiHandle/CategoryApiTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
-public class CategoryApiHandleTest {
+public class CategoryApiTest {
 
     @Mock
     private CategoryRepository categoryRepository;
